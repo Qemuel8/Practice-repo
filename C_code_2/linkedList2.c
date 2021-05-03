bea0d31f02b06f211aee42237fb139ed9ba59ee2
Yigit Alsancak <yigitalsancak8@gmail.com>
2021-05-03 12:41:06 +0300

    Added code to linkedList2

--- C_code_2/linkedList2.c
+++ C_code_2/linkedList2.c
@@ -11,7 +11,13 @@ struct Node* head;
 
 void Print()
 {
-	
+	struct Node* temp = head;
+	while(temp != NULL)
+	{
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+	printf("\n");
 }
 
 void Insert(int data, int n)
@@ -22,14 +28,22 @@ void Insert(int data, int n)
 	if(n == 1)
 	{
 		temp1->next = head;
-		head  =
+		head  = temp1;
+		return;
 	}
+	struct Node* temp2 = head;
+	for(int i = 0;i<n-2;i++)
+	{
+		temp2 = temp2->next;
+	}
+	temp1->next = temp2->next;
+	temp2->next = temp1;
 }
 int main()
 {
 	head = NULL; // initally the list is empty.
 	Insert(2,1); // List: 2
-	Insert 3,2); //List: 2,3
+	Insert(3,2); //List: 2,3
 	Insert(4,1); //List: 4,2,3
 	Insert(5,2); //List: 4,5,2,3
 	Print();
