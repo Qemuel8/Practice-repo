54ddb84f22f71f17aac08cc9993bb2bae34af764
Yigitt Alsancak <yigitalsancak8@gmail.com>
2021-10-12 10:11:09 +0300

    test code added

---
+++ C_code_2/testUygulamasi.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int function(int num1, int num2)
+{
+    int num3;
+    printf("selam bir sayi gir \n");
+    scanf("%d", &num3);
+    return num3 * (num1 + num2);
+
+}
+
+int main()
+{
+
+    int num4 = function(2,5);
+    printf("sayiniz %d", num4);
+
+    return 0;
+}
