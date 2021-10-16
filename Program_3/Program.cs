ec3a11b5b4b839c53dd4c073e297a0324697d5df
Yigitt Alsancak <yigitalsancak8@gmail.com>
2021-10-16 18:16:26 +0300

    Added c# code practices

---
+++ Program_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Program_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cubedNumber = cube(5);
+            Console.WriteLine(cubedNumber);
+            Console.WriteLine(cube(5));
+        }
+
+        static int cube(int num)
+        {
+            int result = num * num * num;
+            return result;
+        }
+    }
+}
---
+++ program2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Yigit", 24);
+             SayHi("Samil", 22);
+              SayHi("Baris", 20);
+            Console.ReadLine();
+        }
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " you are : " + age);
+        }
+    }
+}
