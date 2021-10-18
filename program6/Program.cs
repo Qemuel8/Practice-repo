f499543704e77a27811cdce7ddad7877ee43c718
Yigitt Alsancak <yigitalsancak8@gmail.com>
2021-10-18 11:48:22 +0300

    adding code

---
+++ program6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace program6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int [,] numberGrid = 
+            {
+                {1, 2},
+                {3, 4},
+                {5, 6}
+            };
+            */
+
+            /*Console.Write("Enter a number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter antoher Number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(num1/num2);
+
+             try
+             {
+            Console.Write("Enter a number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter antoher Number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1/num2);
+             }
+             catch(DivideByZeroException e)
+             {
+                 Console.WriteLine(e.Message);
+             }
+             catch(FormatException e)
+             {
+                 Console.WriteLine(e.Message);
+             }
+             */
+
+             
+            
+        }
+    }
+}
